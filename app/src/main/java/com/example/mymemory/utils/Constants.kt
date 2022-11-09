5ca727880580fc9396892e632c89c87c98966196
QD1221 <zxcvbnm12122000@gmail.com>
2022-11-09 06:17:45 -0800

    Init commit

---
+++ app/src/main/java/com/example/mymemory/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.example.mymemory.utils
+
+import com.example.mymemory.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_airplane,
+    R.drawable.ic_car,
+    R.drawable.ic_bike,
+    R.drawable.ic_cart,
+    R.drawable.ic_face,
+    R.drawable.ic_flower,
+    R.drawable.ic_home,
+    R.drawable.ic_machine,
+    R.drawable.ic_paid,
+    R.drawable.ic_park,
+    R.drawable.ic_podcasts,
+    R.drawable.ic_point,
+)
